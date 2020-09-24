77c2cac777520cf25350885cfa70c09c576cdbde
JoeTan <tjwjoe1998@gmail.com>
2020-09-25 01:45:15 +0800

    Fix Checkstyle violations yet once again

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-
 import java.util.Set;
 
 import seedu.address.logic.commands.AddCommand;
